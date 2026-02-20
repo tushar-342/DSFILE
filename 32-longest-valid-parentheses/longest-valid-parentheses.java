33ec15a14f27aaa3d17ad54e5e129e0d1d7214cc
Tushar Kumar <tusharkrverma224@gmail.com>
2026-02-20 09:00:57 +0530

    Time: 5 ms (79.93%) | Memory: 46.7 MB (24.93%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);   // Base for calculation
+        
+        int maxLength = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+        
+        return maxLength;
+    }
+}

b26830f94fef0de2cbfb0d4641b6cfcedf232c15
Tushar Kumar <tusharkrverma224@gmail.com>
2026-03-11 19:06:48 +0530

    Time: 6 ms (42.95%) | Memory: 48.7 MB (13.21%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,22 +1,67 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] res = new int[nums.length];
-        int i = 0;
-        int j = nums.length - 1;
-        int k = nums.length - 1;
-        
-        while(i<=j){
-            if(Math.abs(nums[i]) > Math.abs(nums[j])){
-                res[k] = nums[i] * nums[i];
-                i = i+1;
-            }else{
-                res[k] = nums[j] * nums[j];
-                j = j-1;
+        int n = nums.length;
+        List<Integer> neg = new ArrayList<>();
+        List<Integer> pos = new ArrayList<>();
+
+        // Separate negative and positive numbers
+        for (int num : nums) {
+            if (num < 0)
+                neg.add(num);
+            else
+                pos.add(num);
+        }
+
+        // Case 1: No negative numbers
+        if (neg.size() == 0) {
+            for (int i = 0; i < pos.size(); i++)
+                pos.set(i, pos.get(i) * pos.get(i));
+            return pos.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        // Case 2: No positive numbers
+        if (pos.size() == 0) {
+            for (int i = 0; i < neg.size(); i++)
+                neg.set(i, neg.get(i) * neg.get(i));
+            Collections.reverse(neg);
+            return neg.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        // Case 3: Both negative and positive exist
+        int i = 0, j = 0, id = 0;
+        int n1 = neg.size();
+        int n2 = pos.size();
+        int[] res = new int[n1 + n2];
+
+        // Square negatives and reverse them
+        for (i = 0; i < n1; i++)
+            neg.set(i, neg.get(i) * neg.get(i));
+        Collections.reverse(neg);
+
+        // Square positives
+        for (i = 0; i < n2; i++)
+            pos.set(i, pos.get(i) * pos.get(i));
+
+        // Merge two sorted lists
+        i = 0; // j=0
+        j=0;
+        while (i < n1 && j < n2) {
+            if (neg.get(i) <= pos.get(j)) {
+                res[id++] = neg.get(i++);
+                // res[id]=neg[i]
+                // id++;
+                // i++
+            } else {
+                res[id++] = pos.get(j++);
             }
-            k = k-1;
-        }
-            return res;
-        
-        }
-        
+        } // while loop khatam
+
+        while (i < n1)
+            res[id++] = neg.get(i++);
+
+        while (j < n2)
+            res[id++] = pos.get(j++);
+
+        return res;
     }
+}

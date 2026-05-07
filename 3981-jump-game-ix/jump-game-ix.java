006fbcf7c65673ef50171ab0b5937be6c6dbd69b
Tushar Kumar <tusharkrverma224@gmail.com>
2026-05-07 14:25:06 +0530

    Time: 4 ms (100.00%) | Memory: 198.6 MB (61.29%) - LeetSync

---
+++ 3981-jump-game-ix/jump-game-ix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] maxValue(int[] nums) {
+
+        int n = nums.length;
+
+        int[] ans = new int[n];
+        int[] preMax = new int[n];
+
+        preMax[0] = nums[0];
+
+        // Prefix maximum
+        for(int i = 1; i < n; i++) {
+            preMax[i] = Math.max(preMax[i - 1], nums[i]);
+        }
+
+        int sufMin = Integer.MAX_VALUE;
+
+        // Traverse from right
+        for(int i = n - 1; i >= 0; i--) {
+
+            if(preMax[i] > sufMin) {
+                ans[i] = (i == n - 1) ? preMax[i] : ans[i + 1];
+            }
+            else {
+                ans[i] = preMax[i];
+            }
+
+            sufMin = Math.min(sufMin, nums[i]);
+        }
+
+        return ans;
+    }
+}
